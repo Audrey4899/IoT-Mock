909625d870004dc43e61b2a853b187fcff84d197
thfague <faguethibault@gmail.com>
2019-11-06 13:34:21 +0100

    add json generator method

--- rules-generator/generator.rb
+++ rules-generator/generator.rb
@@ -3,24 +3,25 @@
 require 'json'
 
 class Rule
-    @@array = Array.new
-    attr_reader :type, :timeout, :repeat, :interval
+    attr_reader :type
     attr_reader :request_method, :request_path, :request_headers, :request_body
     attr_reader :response_status, :response_headers, :response_body
 
-    def self.all_instances
-        @@array
-    end
-
-    def initialize(type)
-        @type = type
-        @@array << self
+    def initialize(rule_type, request_method, request_path, request_headers, request_body, response_status, response_headers, response_body)
+        @type = rule_type
+        @request_method = request_method
+        @request_path = request_path
+        @request_headers = request_headers
+        @request_body = request_body
+        @response_status = response_status
+        @response_headers = response_headers
+        @response_body = response_body
     end
 end
 
 class Generator
     attr_reader :file_name, :file_type
-    attr_accessor :file_data, :host, :paths
+    attr_accessor :file_data, :host, :paths, :rules
     attr_accessor :request_host, :request_dest, :request_method, :request_path, :request_headers, :request_body
     attr_accessor :response_host, :response_dest, :response_status, :response_headers, :response_body
 
@@ -36,10 +37,10 @@ class Generator
         file_data.each do |line|
             address1 = line.match(/Host=(([0-9]{1,3}\.)+[0-9]{1,3})/)
             address2 = line.match(/Dest=(([0-9]{1,3}\.)+[0-9]{1,3})/)
-            if !paths.include? address1[1]
+            unless paths.include? address1[1]
                 @paths << address1[1]
             end
-            if !paths.include? address2[1]
+            unless paths.include? address2[1]
                 @paths << address2[1]
             end
         end
@@ -62,38 +63,51 @@ class Generator
 
     def get_request_composition(requests)
         request = requests[0].match(/(Host=(([0-9]{1,3}\.)+[0-9]{1,3})).*(Dest=(([0-9]{1,3}\.)+[0-9]{1,3})).*(Verb=([A-Z]+)).([^\s]+)/)
-        @request_host = request[1]
-        @request_dest = request[4]
-        @request_method = request[7]
-        @request_path = request[8]
+        @request_host = request[2]
+        @request_dest = request[5]
+        @request_method = request[8]
+        @request_path = request[9]
 
-        if !requests[0].match(/HTTP\/.+\s\((.*)\)/).nil?
+        unless requests[0].match(/HTTP\/.+\s\((.*)\)/).nil?
             request = requests[0].match(/HTTP\/.+\s\((.*)\)/)
-            @request_headers = request[0]
+            @request_headers = request[1]
         end
-        if !requests[0].match(/;;(.*)/).nil?
+        unless requests[0].match(/;;(.*)/).nil?
             request = requests[0].match(/;;(.*)/)
-            @request_body = request[0]
+            @request_body = request[1]
         end
     end
 
     def get_response_composition(responses)
         response = responses[0].match(/(Host=(([0-9]{1,3}\.)+[0-9]{1,3})).*(Dest=(([0-9]{1,3}\.)+[0-9]{1,3})).*(status=([0-9]{3}))/)
-        @response_host = response[1]
-        @response_dest = response[4]
-        @response_status = response[7]
+        @response_host = response[2]
+        @response_dest = response[5]
+        @response_status = response[8]
 
-        if !responses[0].match(/HTTP\/.+\s\((.*)\)/).nil?
+        unless responses[0].match(/HTTP\/.+\s\((.*)\)/).nil?
             response = responses[0].match(/HTTP\/.+\s\((.*)\)/)
-            @response_headers = response[0]
+            @response_headers = response[1]
         end
-        if !responses[0].match(/;;(.*)/).nil?
+        unless responses[0].match(/;;(.*)/).nil?
             response = responses[0].match(/;;(.*)/)
-            @response_body = response[0]
+            @response_body = response[1]
         end
     end
 
+    def rules_to_json
+        rules_hash = Array.new
+        rules.each do |rule|
+            hash = {}
+            rule.instance_variables.each { |var|
+                hash[var.to_s.delete("@")] = rule.instance_variable_get(var)
+            }
+            rules_hash << hash
+        end
+        puts JSON.generate(rules_hash)
+    end
+
     def create_rules
+        @host = "192.168.43.76" #To test code(only with "brut" file)
         requests = Array.new
         responses = Array.new
         file_data.each do |line|
@@ -102,21 +116,34 @@ class Generator
             elsif line.include? "status="
                 responses << line
             else
-                puts "Error when creating rules."
+                puts "Error when dispatching resquests/responses."
                 exit
             end
         end
+        @rules = Array.new
+        idx = 0
         while requests.length != 0 && responses.length != 0
             get_request_composition(requests)
             get_response_composition(responses)
             if request_dest == host && response_host == host #=> rule inOut
-                Rule.new("inOut", request_path, request_method, response_status, response_headers, response_body)
-            elsif request_host == host && response_dest == host #=> rule outIn
-                Rule.new("outIn", request_dest + request_path , request_method)
+                rule_type = "inOut"
+            elsif request_host == host #=> rule out
+                rule_type = "outIn"
+                response_status = nil
+                response_headers = nil
+                response_body = nil
+            else
+                puts "Error when creating rules(rule type unknown)."
+                exit
+            end
+            rule = Rule.new(rule_type, request_method, request_path, request_headers, request_body, response_status, response_headers, response_body)
+            unless rules.include? rule
+                @rules << rule
             end
             requests.shift
             responses.shift
         end
+        rules_to_json
     end
 
     def start(path)
