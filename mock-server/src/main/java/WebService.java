662c240d0ecca59d982228ea2c8d4d527ba37c9c
romain_ncls <nicolasromain0@gmail.com>
2019-11-08 09:32:23 +0100

    add Loader & LoaderException, make YamlLoader implement Loader & throw LoaderException, implement route addRules, add exception mapping

--- mock-server/src/main/java/WebService.java
+++ mock-server/src/main/java/WebService.java
@@ -1,5 +1,13 @@
 import io.javalin.Javalin;
+import io.javalin.http.Handler;
+import io.javalin.http.HandlerType;
+import load.Loader;
+import load.LoaderException;
+import load.YamlLoader;
 import model.Component;
+import model.InOutRule;
+import model.OutInRule;
+import model.Rule;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @WebServlet(urlPatterns = {"/*"})
 public class WebService extends HttpServlet {
@@ -16,8 +25,46 @@ public class WebService extends HttpServlet {
 
     public WebService() {
         app = Javalin.createStandalone();
+        app.exception(LoaderException.class, (exception, ctx) -> {
+            ctx.status(400);
+            ctx.result(List.of(exception.getClass().toString(), (exception.getMessage() != null) ? exception.getMessage() : "").toString());
+        });
+        app.exception(Exception.class, (exception, ctx) -> {
+            exception.printStackTrace();
+            ctx.status(500);
+            ctx.result(List.of(exception.getClass().toString(), (exception.getMessage() != null) ? exception.getMessage() : "").toString());
+        });
 
         app.get("/", ctx -> ctx.result("It works !"));
+        app.post("/addRules", addRulesHandler());
+    }
+
+    private Handler addRulesHandler() {
+        return ctx -> {
+            Loader loader;
+            if(Objects.equals(ctx.header("Content-Type"), "text/yaml")) {
+                loader = new YamlLoader();
+            } else {
+                throw new LoaderException("Wrong content type");
+            }
+            List<Rule> rules = loader.load(ctx.body());
+            initRules(rules);
+            ctx.result("Rules added");
+        };
+    }
+
+    private void initRules(List<Rule> rules) {
+        rules.forEach(rule -> {
+            if(rule instanceof InOutRule) {
+                app.addHandler(HandlerType.valueOf(rule.getRequest().getMethod()), rule.getRequest().getPath(), ctx -> {
+                    ctx.status(rule.getResponse().getStatus());
+                    ctx.result(rule.getResponse().getBody());
+                    rule.getResponse().getHeaders().forEach(ctx::header);
+                });
+            } else if(rule instanceof OutInRule) {
+                new OutputRequest((OutInRule) rule).start();
+            }
+        });
     }
 
     @Override
--- mock-server/src/main/java/load/YamlLoader.java
+++ mock-server/src/main/java/load/YamlLoader.java
@@ -1,36 +1,37 @@
+package load;
+
 import model.*;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.parser.ParserException;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
-import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.parser.ParserException;
 
-
-public class YamlLoader {
+public class YamlLoader implements Loader {
 
     /**
      * Parse and deserialize a Yaml formatted String
      * @param yaml The Yaml formatted String from which to load the rules
      * @return The list of loaded rules
      */
-    public List<Rule> load(String yaml) {
+    public List<Rule> load(String yaml) throws LoaderException {
         List<Rule> rules = new ArrayList<>();
         Yaml parser = new Yaml();
-        List<Map> test;
+        List<Map> parsedRules;
         try {
-            test = parser.load(yaml);
+            parsedRules = parser.load(yaml);
         } catch (ClassCastException e) {
-            throw new IllegalArgumentException("Body must be a list.");
+            throw new LoaderException("Body must be a list.");
         } catch (ParserException e) {
-            throw new IllegalArgumentException(e);
+            throw new LoaderException(e);
         }
 
-        if (test == null) throw new IllegalArgumentException("Body must not be empty");
+        if (parsedRules == null) throw new LoaderException("Body must not be empty");
 
-        test.forEach(r -> {
+        for (Map r: parsedRules) {
             String type = (String) r.get("type");
 
             if (Objects.equals(type, "inOut")) {
@@ -38,9 +39,9 @@ public class YamlLoader {
             } else if (Objects.equals(type, "outIn")) {
                 rules.add(loadOutIn(r));
             } else {
-                throw new IllegalArgumentException();
+                throw new LoaderException();
             }
-        });
+        }
 
         return rules;
     }
@@ -50,11 +51,11 @@ public class YamlLoader {
      * @param rule The parsed rule
      * @return The loaded InOutRule
      */
-    private InOutRule loadInOut(Map rule) {
+    private InOutRule loadInOut(Map rule) throws LoaderException {
         Request req = loadRequest(rule);
         Response res = loadResponse(rule);
-        if (req == null || res == null) throw new IllegalArgumentException();
-        if(!req.getPath().startsWith("/")) throw new IllegalArgumentException(String.format("Wrong path format: '%s'. Must start with /", req.getPath()));
+        if (req == null || res == null) throw new LoaderException();
+        if(!req.getPath().startsWith("/")) throw new LoaderException(String.format("Wrong path format: '%s'. Must start with /", req.getPath()));
         return new InOutRule(req, res);
     }
 
@@ -63,11 +64,11 @@ public class YamlLoader {
      * @param rule The parsed rule
      * @return The loaded InOutRule
      */
-    private OutInRule loadOutIn(Map rule) {
+    private OutInRule loadOutIn(Map rule) throws LoaderException {
         Request req = loadRequest(rule);
         Response res = loadResponse(rule);
-        if (req == null) throw new IllegalArgumentException();
-        if(!req.getPath().matches("^https?://.*$")) throw new IllegalArgumentException(String.format("Wrong path format: '%s'. Must start with http:// or https://", req.getPath()));
+        if (req == null) throw new LoaderException();
+        if(!req.getPath().matches("^https?://.*$")) throw new LoaderException(String.format("Wrong path format: '%s'. Must start with http:// or https://", req.getPath()));
         Long timeout = getCheckAndCast(rule, "timeout", Long.class);
         Integer repeat = getCheckAndCast(rule, "repeat", Integer.class);
         Long interval = getCheckAndCast(rule, "interval", Long.class);
@@ -79,7 +80,7 @@ public class YamlLoader {
      * @param rule The parsed rule containing the request
      * @return The loaded Request
      */
-    private Request loadRequest(Map rule) {
+    private Request loadRequest(Map rule) throws LoaderException {
         Map req = getCheckAndCast(rule, "req", Map.class);
         if (req == null) return null;
 
@@ -95,7 +96,7 @@ public class YamlLoader {
      * @param rule The parsed rule containing the response
      * @return The loaded Response
      */
-    private Response loadResponse(Map rule) {
+    private Response loadResponse(Map rule) throws LoaderException {
         Map res = getCheckAndCast(rule, "res", Map.class);
         if (res == null) return null;
 
@@ -113,11 +114,11 @@ public class YamlLoader {
      * @param <T> The type of the value to return
      * @return The casted value
      */
-    private <T> T getCheckAndCast(Map m, String key, Class<T> type) {
+    private <T> T getCheckAndCast(Map m, String key, Class<T> type) throws LoaderException {
         try {
             return type.cast(m.get(key));
         } catch (ClassCastException e) {
-            throw new IllegalArgumentException(String.format("Wrong type for '%s', expected %s", key, type.getSimpleName()));
+            throw new LoaderException(String.format("Wrong type for '%s', expected %s", key, type.getSimpleName()));
         }
     }
 
@@ -129,9 +130,9 @@ public class YamlLoader {
      * @param <T> The type of the value to return
      * @return The casted non-null value
      */
-    private <T> T getCheckAndCastNotNull(Map m, String key, Class<T> type) {
+    private <T> T getCheckAndCastNotNull(Map m, String key, Class<T> type) throws LoaderException {
         T t = getCheckAndCast(m, key, type);
-        if (t == null) throw new IllegalArgumentException(String.format("Missing parameter '%s'", key));
+        if (t == null) throw new LoaderException(String.format("Missing parameter '%s'", key));
         return t;
     }
 }
