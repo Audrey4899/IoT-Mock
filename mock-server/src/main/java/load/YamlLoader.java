90c58de2a3aeb705be5de13b506b9a7d87e5676d
romain_ncls <nicolasromain0@gmail.com>
2019-11-12 16:21:36 +0100

    move yaml keys to static attributes in YamlLoader

--- mock-server/src/main/java/load/YamlLoader.java
+++ mock-server/src/main/java/load/YamlLoader.java
@@ -12,8 +12,24 @@ import java.util.Objects;
 
 public class YamlLoader implements Loader {
 
+    private static final String KEY_TYPE = "type";
+    private static final String KEY_TIMEOUT = "timeout";
+    private static final String KEY_REPEAT = "repeat";
+    private static final String KEY_INTERVAL = "interval";
+    private static final String KEY_REQUEST = "req";
+    private static final String KEY_RESPONSE = "res";
+    private static final String KEY_METHOD = "method";
+    private static final String KEY_STATUS = "status";
+    private static final String KEY_PATH = "path";
+    private static final String KEY_HEADERS = "headers";
+    private static final String KEY_BODY = "body";
+
+    private static final String VALUE_INOUT = "inOut";
+    private static final String VALUE_OUTIN = "outIn";
+
     /**
      * Parse and deserialize a Yaml formatted String
+     *
      * @param yaml The Yaml formatted String from which to load the rules
      * @return The list of loaded rules
      */
@@ -31,15 +47,15 @@ public class YamlLoader implements Loader {
 
         if (parsedRules == null) throw new LoaderException("Body must not be empty");
 
-        for (Map r: parsedRules) {
-            String type = (String) r.get("type");
+        for (Map r : parsedRules) {
+            String type = (String) r.get(KEY_TYPE);
 
-            if (Objects.equals(type, "inOut")) {
+            if (Objects.equals(type, VALUE_INOUT)) {
                 rules.add(loadInOut(r));
-            } else if (Objects.equals(type, "outIn")) {
+            } else if (Objects.equals(type, VALUE_OUTIN)) {
                 rules.add(loadOutIn(r));
             } else {
-                throw new LoaderException();
+                throw new LoaderException("Unsupported rule type");
             }
         }
 
@@ -48,75 +64,82 @@ public class YamlLoader implements Loader {
 
     /**
      * Load an InOutRule
+     *
      * @param rule The parsed rule
      * @return The loaded InOutRule
      */
     private InOutRule loadInOut(Map rule) throws LoaderException {
         Request req = loadRequest(rule);
         Response res = loadResponse(rule);
-        if (req == null || res == null) throw new LoaderException();
-        if(!req.getPath().startsWith("/")) throw new LoaderException(String.format("Wrong path format: '%s'. Must start with /", req.getPath()));
+        if (req == null || res == null) throw new LoaderException("Missing request or response");
+        if (!req.getPath().startsWith("/"))
+            throw new LoaderException(String.format("Wrong path format: '%s'. Must start with /", req.getPath()));
         return new InOutRule(req, res);
     }
 
     /**
      * Load an OutInRule
+     *
      * @param rule The parsed rule
      * @return The loaded InOutRule
      */
     private OutInRule loadOutIn(Map rule) throws LoaderException {
         Request req = loadRequest(rule);
         Response res = loadResponse(rule);
-        if (req == null) throw new LoaderException();
-        if(!req.getPath().matches("^https?://.*$")) throw new LoaderException(String.format("Wrong path format: '%s'. Must start with http:// or https://", req.getPath()));
-        Long timeout = getCheckAndCast(rule, "timeout", Long.class);
-        Integer repeat = getCheckAndCast(rule, "repeat", Integer.class);
-        Long interval = getCheckAndCast(rule, "interval", Long.class);
+        if (req == null) throw new LoaderException("Missing request");
+        if (!req.getPath().matches("^https?://.*$"))
+            throw new LoaderException(String.format("Wrong path format: '%s'. Must start with http:// or https://", req.getPath()));
+        Long timeout = getCheckAndCast(rule, KEY_TIMEOUT, Long.class);
+        Integer repeat = getCheckAndCast(rule, KEY_REPEAT, Integer.class);
+        Long interval = getCheckAndCast(rule, KEY_INTERVAL, Long.class);
         return new OutInRule(req, res, (timeout != null) ? timeout : 0, (repeat != null) ? repeat : 1, (interval != null) ? interval : 1000);
     }
 
     /**
      * Load a Request
+     *
      * @param rule The parsed rule containing the request
      * @return The loaded Request
      */
     private Request loadRequest(Map rule) throws LoaderException {
-        Map req = getCheckAndCast(rule, "req", Map.class);
+        Map req = getCheckAndCast(rule, KEY_REQUEST, Map.class);
         if (req == null) return null;
 
-        String method = getCheckAndCastNotNull(req, "method", String.class);
-        String path = getCheckAndCastNotNull(req, "path", String.class);
-        Map<String, String> headers = getCheckAndCast(req, "headers", Map.class);
-        String body = getCheckAndCast(req, "body", String.class);
+        String method = getCheckAndCastNotNull(req, KEY_METHOD, String.class);
+        String path = getCheckAndCastNotNull(req, KEY_PATH, String.class);
+        Map<String, String> headers = getCheckAndCast(req, KEY_HEADERS, Map.class);
+        String body = getCheckAndCast(req, KEY_BODY, String.class);
         return new Request(method, path, headers, body);
     }
 
     /**
      * Load a Response
+     *
      * @param rule The parsed rule containing the response
      * @return The loaded Response
      */
     private Response loadResponse(Map rule) throws LoaderException {
-        Map res = getCheckAndCast(rule, "res", Map.class);
+        Map res = getCheckAndCast(rule, KEY_RESPONSE, Map.class);
         if (res == null) return null;
 
-        Integer status = getCheckAndCastNotNull(res, "status", Integer.class);
-        Map<String, String> headers = getCheckAndCast(res, "headers", Map.class);
-        String body = getCheckAndCast(res, "body", String.class);
+        Integer status = getCheckAndCastNotNull(res, KEY_STATUS, Integer.class);
+        Map<String, String> headers = getCheckAndCast(res, KEY_HEADERS, Map.class);
+        String body = getCheckAndCast(res, KEY_BODY, String.class);
         return new Response(status, headers, body);
     }
 
     /**
      * Get the key's value from the map and cast it
-     * @param m The map to get the value from
-     * @param key The get of the value to get
+     *
+     * @param m    The map to get the value from
+     * @param key  The get of the value to get
      * @param type The type of the value to return
-     * @param <T> The type of the value to return
+     * @param <T>  The type of the value to return
      * @return The casted value
      */
     private <T> T getCheckAndCast(Map m, String key, Class<T> type) throws LoaderException {
         try {
-            if(m.get(key) instanceof Integer && type.equals(Long.class)) {
+            if (m.get(key) instanceof Integer && type.equals(Long.class)) {
                 return type.cast(((Integer) m.get(key)).longValue());
             }
             return type.cast(m.get(key));
@@ -127,10 +150,11 @@ public class YamlLoader implements Loader {
 
     /**
      * Get the key's value from the map and cast it
-     * @param m The map to get the value from
-     * @param key The get of the value to get
+     *
+     * @param m    The map to get the value from
+     * @param key  The get of the value to get
      * @param type The type of the value to return
-     * @param <T> The type of the value to return
+     * @param <T>  The type of the value to return
      * @return The casted non-null value
      */
     private <T> T getCheckAndCastNotNull(Map m, String key, Class<T> type) throws LoaderException {
