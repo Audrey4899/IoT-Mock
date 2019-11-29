af33cdc8d83dce9e577f40489ff5df6567be10b9
thfague <faguethibault@gmail.com>
2019-11-29 15:03:39 +0100

    add checks in JsonLoader

--- mock-esp/src/load/JsonLoader.cpp
+++ mock-esp/src/load/JsonLoader.cpp
@@ -12,18 +12,20 @@ String JsonLoader::load(String str, std::list<Rule *> &rules) {
     JsonObject object = json[i];
     if (object.isNull()) return "LoaderError: Rule must be an object.";
     String type = object["type"].as<String>();
+    String ok;
 
     if (type.equals("inout")) {
       InOutRule *rule;
-      loadInOutRule(object, rule);
+      ok = loadInOutRule(object, rule);
       rules.push_back(rule);
     } else if (type.equals("outin")) {
       OutInRule *rule;
-      loadOutInRule(object, rule);
+      ok = loadOutInRule(object, rule);
       rules.push_back(rule);
     } else {
-      return "LoaderError: Unusupported rule type.";
+      return "LoaderError: Unsupported rule type.";
     }
+    if(!ok.equals("OK")) return ok;
   }
 
   return "OK";
@@ -32,10 +34,14 @@ String JsonLoader::load(String str, std::list<Rule *> &rules) {
 String JsonLoader::loadInOutRule(JsonObject &rule, InOutRule *&inOutRule) {
   Request *request;
   Response *response;
-  loadRequest(rule, request);
-  loadResponse(rule, response);
-  // TODO: check OK
+  String ok = loadRequest(rule, request);
+  if(!ok.equals("OK")) return ok;
+  ok = loadResponse(rule, response);
+  if(!ok.equals("OK")) return ok;
+  if(request==nullptr) return "LoaderError: Request is required.";
+  if(response==nullptr) return "LoaderError: Response is required.";
 
+  if(request->getPath().charAt(0) != '/') return "LoaderError: InOutRule Request path must start with '/'.";
   inOutRule = new InOutRule(*request, *response);
   return "OK";
 }
@@ -43,37 +49,47 @@ String JsonLoader::loadInOutRule(JsonObject &rule, InOutRule *&inOutRule) {
 String JsonLoader::loadOutInRule(JsonObject &rule, OutInRule *&outInRule) {
   Request *request;
   Response *response;
-  loadRequest(rule, request);
-  loadResponse(rule, response);
-  long timeout = rule["timeout"].as<long>();
-  int repeat = rule["repeat"].as<int>();
-  long interval = rule["interval"].as<long>();
-  // TODO: check OK
+  String ok = loadRequest(rule, request);
+  if(!ok.equals("OK")) return ok;
+  ok = loadResponse(rule, response);
+  if(!ok.equals("OK")) return ok;
+  if(request==nullptr) return "LoaderError: Request is required.";
+
+  long timeout = (rule["timeout"].is<long>())? rule["timeout"].as<long>(): 0;
+  int repeat = (rule["repeat"].is<int>())? rule["repeat"].as<int>(): 1;
+  long interval = (rule["interval"].is<long>())? rule["interval"].as<long>(): 1000;
+  
+  String proto = request->getPath().substring(0, 7);
+  if(!proto.equals("http://") && !proto.equals("https:/")) return "LoaderError: OutInRules Request path must start with 'http' or 'https'.";
+  
   outInRule = new OutInRule(*request, *response, timeout, repeat, interval);
   return "OK";
 }
 
 String JsonLoader::loadRequest(JsonObject &rule, Request *&request) {
   JsonObject req = rule["request"];
+  if(req.isNull()) return "OK";
   String method = req["method"].as<String>();
+  method = method.equals("null") ? "" : method;
   String path = req["path"].as<String>();
   std::map<String, String> *headers;
   loadHeaders(req, headers);
   String body = req["body"].as<String>();
-  // TODO: check OK
-
+  body = body.equals("null") ? "" : body;
+  if(method.equals("")) return "LoaderError: Method/Verb cannot be empty.";
   request = new Request(method, path, *headers, body);
   return "OK";
 }
 
 String JsonLoader::loadResponse(JsonObject &rule, Response *&response) {
   JsonObject res = rule["response"];
+  if(res.isNull()) return "OK";
   int status = res["status"].as<int>();
   std::map<String, String> *headers;
   loadHeaders(res, headers);
   String body = res["body"].as<String>();
-  // TODO: check OK
-
+  body = body.equals("null") ? "" : body;
+  if(status < 100 || status >= 600) return "LoaderError: Status must be between 100 and 600.";
   response = new Response(status, *headers, body);
   return "OK";
 }
