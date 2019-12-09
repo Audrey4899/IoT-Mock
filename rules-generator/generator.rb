a47d1f862080af6542b670b6c0070fbc8aa5142a
thfague <faguethibault@gmail.com>
2019-12-09 11:39:51 +0100

    fix trim headers values and multiple "=" in values

--- rules-generator/generator.rb
+++ rules-generator/generator.rb
@@ -71,31 +71,31 @@ class Generator
     @file_data = File.open(path).readlines.map(&:chomp)
     @paths = []
     @file_data.each do |line|
-      host_adress = line.match(/Host=(([0-9]{1,3}\.)+[0-9]{1,3})/)
-      dest_adress = line.match(/Dest=(([0-9]{1,3}\.)+[0-9]{1,3})/)
-      @paths << host_adress[1] unless paths.include?(host_adress[1])
-      @paths << dest_adress[1] unless paths.include?(dest_adress[1])
+      host_address = line.match(/Source=(([0-9]{1,3}\.)+[0-9]{1,3})/)
+      dest_address = line.match(/Dest=(([0-9]{1,3}\.)+[0-9]{1,3})/)
+      @paths << host_address[1] unless paths.include?(host_address[1])
+      @paths << dest_address[1] unless paths.include?(dest_address[1])
     end
   end
 
-  def ask_hostadress
-    puts 'Choose the host adress: '
+  def ask_hostaddress
+    puts 'Choose the host address: '
     @paths.each { |line| puts line }
     puts ''
     @host = $stdin.gets.chomp
     until paths.include?(host)
-      puts "Error: the chosen adress doesn't exist.\nChoose the host adress: "
+      puts "Error: the chosen address doesn't exist.\nChoose the host address: "
       paths.each { |line| puts line }
       puts ''
       @host = $stdin.gets.chomp
       puts ''
     end
-    puts "=> #{@host} has been chosen like host adress."
+    puts "=> #{@host} has been chosen as host address."
   end
 
   def get_request_composition(requests)
     request = requests[0].split('@')
-    @request_label = request[0][/^.*\(/].tr('(', '')
+    @request_label = request[0][/^.*\(/].tr('(', '').strip
     request[0][/^.*\(/] = ''
     request[request.length - 1][/\)/] = ''
     key_value = {}
@@ -103,58 +103,62 @@ class Generator
       if val.split('=')[0] == 'Uri'
         key_value['Uri'] = val.split('Uri=')[1]
       else
-        key_value[val.split('=')[0]] = val.split('=')[1]
+        vals = val.split('=')
+        vals.delete_at(0)
+        key_value[val.split('=')[0]] = vals.join('=')
       end
     end
-    @request_host = key_value['Host']
-    key_value.delete('Host')
-    @request_dest = key_value['Dest']
+    @request_host = key_value['Source'].strip
+    key_value.delete('Source')
+    @request_dest = key_value['Dest'].strip
     key_value.delete('Dest')
-    @request_method = key_value['Verb']
+    @request_method = key_value['Verb'].strip
     key_value.delete('Verb')
-    @request_path = key_value['Uri']
+    @request_path = key_value['Uri'].strip
     key_value.delete('Uri')
     @request_headers = {}
     @request_body = nil
     until key_value.empty?
       if key_value.keys[0] == 'contents'
-        @request_body = key_value.values[0]
+        @request_body = key_value.values[0].strip
       else
-        @request_headers[key_value.keys[0]] = key_value.values[0]
+        @request_headers[key_value.keys[0].strip] = key_value.values[0].strip
       end
-      key_value.delete(key_value.keys[0])
+      key_value.delete(key_value.keys[0].strip)
     end
     @request_headers = nil if @request_headers.empty?
   end
 
   def get_response_composition(responses)
     response = responses[0].split('@')
-    @response_label = response[0][/^.*\(/].tr('(', '')
+    @response_label = response[0][/^.*\(/].tr('(', '').strip
     response[0][/^.*\(/] = ''
     response[response.length - 1][/\)/] = ''
     key_value = {}
-    response.each do |string|
-      if string.split('=')[0] == 'contents'
-        key_value['contents'] = string.split('contents=')[1]
+    response.each do |val|
+      if val.split('=')[0] == 'contents'
+        key_value['contents'] = val.split('contents=')[1]
       else
-        key_value[string.split('=')[0]] = string.split('=')[1]
+        vals = val.split('=')
+        vals.delete_at(0)
+        key_value[val.split('=')[0]] = vals.join('=')
       end
     end
-    @response_host = key_value['Host']
-    key_value.delete('Host')
-    @response_dest = key_value['Dest']
+    @response_host = key_value['Source'].strip
+    key_value.delete('Source')
+    @response_dest = key_value['Dest'].strip
     key_value.delete('Dest')
-    @response_status = key_value['status']
+    @response_status = key_value['status'].strip
     key_value.delete('status')
-    @response_response = key_value['response']
+    @response_response = key_value['response'].strip if !key_value['response'].nil?
     key_value.delete('response')
     @response_headers = {}
     @response_body = nil
     until key_value.empty?
       if key_value.keys[0] == 'contents'
-        @response_body = key_value.values[0]
+        @response_body = key_value.values[0].strip
       else
-        @response_headers[key_value.keys[0]] = key_value.values[0]
+        @response_headers[key_value.keys[0].strip] = key_value.values[0].strip
       end
       key_value.delete(key_value.keys[0])
     end
@@ -194,24 +198,23 @@ class Generator
       get_response_composition(responses)
       if @request_dest == @host && @response_host == @host
         @rule_type = 'inout'
-      elsif request_host == host
+      elsif @request_host == @host
         @rule_type = 'outin'
         @response_status = nil
         @response_headers = nil
         @response_body = nil
       else
-        puts 'Error when creating rules(rule type unknown).'
-        exit
+        @rule_type = ''
       end
       if @rule_type == 'outin'
         fullpath = "http://#{@request_dest}#{request_path}"
         rule = Rule.new(@rule_type, @request_method, fullpath, @request_headers, @request_body, @response_status, @response_headers, @response_body)
-      else
+      elsif @rule_type == 'inout'
         rule = Rule.new(@rule_type, @request_method, @request_path, @request_headers, @request_body, @response_status, @response_headers, @response_body)
       end
-      if @rules.empty?
+      if @rule_type != '' && @rules.empty?
         @rules << rule
-      else
+      elsif @rule_type != '' && !@rules.empty?
         exist = false
         @rules.each do |existing_rule|
           if existing_rule.equals?(rule)
@@ -229,23 +232,23 @@ class Generator
 
   def start(path)
     read_file(path)
-    ask_hostadress if @host == ''
+    ask_hostaddress if @host == ''
     create_rules
   end
 end
 
 # Main
 filepath = nil
-hostadress = nil
+hostaddress = nil
 filename = nil
 filetype = nil
 ARGV.each do |arg|
   if arg.include?('-filepath') && filepath.nil?
     filepath = arg.split('=')[1]
-  elsif arg.include?('-hostadress') && hostadress.nil?
-    hostadress = arg.split('=')[1]
-    unless hostadress.match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/)
-      puts "Error: the adress \"#{hostadress}\" isn't correct."
+  elsif arg.include?('-hostaddress') && hostaddress.nil?
+    hostaddress = arg.split('=')[1]
+    unless hostaddress.match(/^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$/)
+      puts "Error: the address \"#{hostaddress}\" isn't correct."
       exit
     end
   elsif arg.include?('-filename') && filename.nil?
@@ -257,7 +260,7 @@ ARGV.each do |arg|
       exit
     end
   else
-    puts "How to use the script: (default destination file: 'rules.json')\n=> generator.rb -filepath=<path_to_file> [-hostadress=<host_adress>] [-filename=<dest_file_name>] [-filetype=<dest_file_type('yaml' or 'json')>)\n"
+    puts "How to use the script: (default destination file: 'rules.json')\n=> generator.rb -filepath=<path_to_file> [-hostaddress=<host_address>] [-filename=<dest_file_name>] [-filetype=<dest_file_type('yaml' or 'json')>)\n"
     exit
   end
 end
@@ -269,7 +272,7 @@ end
 
 filename = 'rules' if filename.nil?
 filetype = 'json' if filetype.nil?
-hostadress = '' if hostadress.nil?
+hostaddress = '' if hostaddress.nil?
 
-generator = Generator.new(filename, filetype, hostadress)
+generator = Generator.new(filename, filetype, hostaddress)
 generator.start(filepath)
